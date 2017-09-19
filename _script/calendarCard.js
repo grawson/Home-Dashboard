4c23098e2edaf50dba780a5a6be9c497ef720b2b
Gavi Rawson <gavirawson@gmail.com>
2017-09-19 15:27:33 -0400

    Dynamic current day

--- _script/calendarCard.js
+++ _script/calendarCard.js
@@ -1,14 +1,22 @@
 var Mustache = require("mustache");
 
-var DOW = ["S", "M", "T", "W", "T", "F", "S"];
+var DOW = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 
 function render() {
     data = {}
 
+    // Current day
+    var today = new Date();
+    data.currentDay = DOW[today.getDay()]
+                    + ", " + MONTHS[today.getMonth()]
+                    + " " + ordinalSuffixOf(today.getDate());
+
     // Days of week
     data.dow = []
     for (var i = 0; i < 7; i++) {
-        data.dow.push({"dow-title": DOW[i]});
+        data.dow.push({"dowTitle": DOW[i].charAt(0)});
     }
 
     // Weeks
@@ -16,18 +24,18 @@ function render() {
     data.weeks = []
     for (var i = 1; i <= 4; i++) {
 
-        data.weeks.push([{"week-number": i}]);
+        data.weeks.push([{"weekNumber": i}]);
 
         // Days
         data.weeks[i-1].days = [];
         for (var j = 0; j < 7; j++) {
-            data.weeks[i-1].days.push({"day-number": currDay});
+            data.weeks[i-1].days.push({"dayNumber": currDay});
             currDay += 1;
 
             // Events
             data.weeks[i-1].days[j].events = []
             for (var k = 0; k < 3; k++) {
-                data.weeks[i-1].days[j].events.push({"event-title": "Event"});
+                data.weeks[i-1].days[j].events.push({"eventTitle": "Event"});
             }
         }
     }
@@ -37,6 +45,14 @@ function render() {
        var rendered = Mustache.render(template, data);
        $('#calendar-card').html(rendered);
     });
+}
+
+function ordinalSuffixOf(i) {
+    var j = i % 10, k = i % 100;
+    if (j == 1 && k != 11) { return i + "st"; }
+    if (j == 2 && k != 12) { return i + "nd"; }
+    if (j == 3 && k != 13) { return i + "rd"; }
+    return i + "th";
 }
 
 window.onload = function(){
