64c604f9baf273fc5ab2c3047f463e436ef10aa5
Yoni Rawson <yrawson96@gmail.com>
2020-02-23 21:48:14 -0500

    time fixes

--- _script/calendarHeader.js
+++ _script/calendarHeader.js
@@ -7,6 +7,10 @@
     
 
 function render() {
+    const DOW = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const MONTHS = ["January", "February", "March", "April", "May", "June", "July",
+                "August", "September", "October", "November", "December"];
+
     var data = {};
     // Current day
     data.currentDay = `${DOW[Date.today().getDay()]}, ${MONTHS[Date.today().getMonth()]} ${ordinalSuffixOf(Date.today().getDate())}, ${(new Date).getHours() % 12 ? (new Date).getHours() % 12 : 12}:${(new Date).getMinutes()}${(new Date).getHours() > 12 ? 'pm' : 'am'}`;
