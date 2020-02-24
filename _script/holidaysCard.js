6d5d90de477512b4649096b9f722aabb9d3d32d8
Yoni Rawson <yrawson96@gmail.com>
2020-02-23 21:17:28 -0500

    scraping changes

--- _script/holidaysCard.js
+++ _script/holidaysCard.js
@@ -2,6 +2,8 @@
 
 const Mustache = require("mustache");
 require('datejs');
+const Cheerio = require('cheerio')
+const Axios = require('axios')
 
 const REQUEST = "http://www.hebcal.com/hebcal/?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&year=now&month=x&ss=on&mf=on&c=on&geo=geoname&geonameid=5128549&m=50&s=off";
 // const REQUEST = "http://www.hebcal.com/hebcal/?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&year=now&month=x&ss=on&mf=on&c=on&geo=geoname&geonameid=5128549&m=50&s=off";
@@ -39,13 +41,73 @@ function render(data) {
     });
 }
 
+
+
+
+async function scrapeData(callback) {
+    var data = {
+        holidays: []
+    }
+
+    var webpage = await Axios.get('https://www.yinr.org/')
+
+    var html = await Cheerio.load(webpage.data)
+
+    var nodes = html('.two-fifth')[0].children[1].firstChild.firstChild
+
+    // this weeks parsha
+    data.parsha = html(html(nodes).find(':contains("Shabbat")')[1]).text()
+
+
+    // candle lighting
+
+    var candle_lighting_node = html(nodes).find(':contains("Candle Lighting")')
+
+    var timeRegex =  /\d{1,2}:\d*pm/
+
+    cLTime = timeRegex.exec(html(candle_lighting_node).text())[0]
+
+    data.holidays.push({
+        title: "Candle Lighting",
+        time: cLTime
+    })
+
+    // Mincha
+    var minchaNodes = html(nodes).find(":contains('Mincha')")
+    var fridayMinchaTime = timeRegex.exec(html(minchaNodes[0]).text())[0]
+
+    data.holidays.push({
+        title: "Friday Mincha",
+        time: fridayMinchaTime
+    })
+
+    var satMinchaTime = timeRegex.exec(html(minchaNodes[2]).text())[0]
+
+    data.holidays.push({
+        title: "Shabbat Mincha",
+        time: satMinchaTime
+    })
+
+    // Havdala
+    var havdalNodes = html(nodes).find(":contains('Shabbat Ends')")
+    havdalaTime = timeRegex.exec(html(havdalNodes).text())[0]
+
+    data.holidays.push({
+        title: "Havdala",
+        time: havdalaTime
+    })
+
+    callback(data)
+}
+
+
 // MAIN **************************************************************************************************************
 
 
 $(function () {
-    loadData(render);
+    scrapeData(render);
     window.setInterval(function() {
         console.log("Reloading holiday data...");
-        loadData(render);
+        scrapeData(render);
     }, REFRESH_RATE*1000);
 });
