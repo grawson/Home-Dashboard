f920de381e146b05944895e6efe302e686a3b5af
Yoni Rawson <yrawson96@gmail.com>
2020-02-23 21:53:04 -0500

    moved function to new file

--- _script/calendarCard.js
+++ _script/calendarCard.js
@@ -223,13 +223,7 @@ function initEvents(dateOfEvent, data, eventData) {
 }
 
 
-function ordinalSuffixOf(i) {
-    var j = i % 10, k = i % 100;
-    if (j == 1 && k != 11) { return i + "st"; }
-    if (j == 2 && k != 12) { return i + "nd"; }
-    if (j == 3 && k != 13) { return i + "rd"; }
-    return i + "th";
-}
+
 
 // set month and year of prev month. day needs to be set depending on use case.
 function prevMonthDate(currDate) {
--- _script/calendarHeader.js
+++ _script/calendarHeader.js
@@ -8,7 +8,14 @@ const Mustache = require("mustache");
         render();
     }, 1000);
  })   
-    
+
+function ordinalSuffixOf(i) {
+    var j = i % 10, k = i % 100;
+    if (j == 1 && k != 11) { return i + "st"; }
+    if (j == 2 && k != 12) { return i + "nd"; }
+    if (j == 3 && k != 13) { return i + "rd"; }
+    return i + "th";
+}
 
 function render() {
     const DOW = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
