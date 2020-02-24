e3f8633bfa5bcbb709242ca0a6f5e1612fc5b850
Yoni Rawson <yrawson96@gmail.com>
2020-02-23 21:57:09 -0500

    wrong file issues

--- _script/calendarHeader.js
+++ _script/calendarHeader.js
@@ -25,7 +25,7 @@ function render() {
     var data = {};
     // Current day
     data.currentDay = `${DOW[Date.today().getDay()]}, ${MONTHS[Date.today().getMonth()]} ${ordinalSuffixOf(Date.today().getDate())}, ${(new Date).getHours() % 12 ? (new Date).getHours() % 12 : 12}:${(new Date).getMinutes()}${(new Date).getHours() > 12 ? 'pm' : 'am'}`;
-    $.get('./_view/calendarCard.mustache', function (template) {
+    $.get('./_view/calendarHeader.mustache', function (template) {
         var rendered = Mustache.render(template, data);
         $('#calendar-header').html(rendered);
     });
