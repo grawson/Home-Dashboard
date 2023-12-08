e5e4454aaf592d7fc47d60cbc7b34c1543537a45
Yoni Rawson <yrawson96@gmail.com>
2023-12-08 11:35:02 +0200

    basic cal grid layout

--- _script/auth/CalendarAuth.js
+++ _script/auth/CalendarAuth.js
@@ -7,67 +7,66 @@ var googleAuth = require('google-auth-library');
 
 // If modifying these scopes, delete your previously saved credentials
 var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
-var TOKEN_DIR = "./_script/auth";
-var TOKEN_PATH = "./access_token.json";
-var SECRET_PATH = "./client_secret.json";
+var TOKEN_DIR = './_script/auth';
+var TOKEN_PATH = './access_token.json';
+var SECRET_PATH = './client_secret.json';
 
 /* MODULE ********************************************************************************/
 
-exports.authorize = function(callback) {
-    var oauth2Client = setup();
-    oauth2Client.credentials = require(TOKEN_PATH);
-    callback(oauth2Client);
+exports.authorize = function () {
+	var oauth2Client = setup();
+	oauth2Client.credentials = require(TOKEN_PATH);
+	return oauth2Client;
 };
 
-
-exports.getNewToken = function(callback) {
-    var oauth2Client = setup();
-    var authUrl = oauth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES
-    });
-    console.log('Authorize this app by visiting this url: ', authUrl);
-    var rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    rl.question('Enter the code from that page here: ', function(code) {
-        rl.close();
-        oauth2Client.getToken(code, function(err, token) {
-            if (err) {
-                console.log('Error while trying to retrieve access token', err);
-                return;
-            }
-            oauth2Client.credentials = token;
-            storeToken(token);
-        });
-    });
+exports.getNewToken = function (callback) {
+	var oauth2Client = setup();
+	var authUrl = oauth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: SCOPES,
+	});
+	console.log('Authorize this app by visiting this url: ', authUrl);
+	var rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+	rl.question('Enter the code from that page here: ', function (code) {
+		rl.close();
+		oauth2Client.getToken(code, function (err, token) {
+			if (err) {
+				console.log('Error while trying to retrieve access token', err);
+				return;
+			}
+			oauth2Client.credentials = token;
+			storeToken(token);
+		});
+	});
 };
 
 /* FUNC ********************************************************************************/
 
 function setup() {
-    var credentials = require(SECRET_PATH);
-    var clientSecret = credentials.installed.client_secret;
-    var clientId = credentials.installed.client_id;
-    var redirectUrl = credentials.installed.redirect_uris[0];
-    var auth = new googleAuth();
-    return new auth.OAuth2(clientId, clientSecret, redirectUrl);
+	var credentials = require(SECRET_PATH);
+	var clientSecret = credentials.installed.client_secret;
+	var clientId = credentials.installed.client_id;
+	var redirectUrl = credentials.installed.redirect_uris[0];
+	var auth = new googleAuth();
+	return new auth.OAuth2(clientId, clientSecret, redirectUrl);
 }
 
 /**
-* Store token to disk be used in later program executions.
-*
-* @param {Object} token The token to store to disk.
-*/
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
 function storeToken(token) {
-    try {
-        fs.mkdirSync(TOKEN_DIR);
-    } catch (err) {
-        if (err.code != 'EEXIST') {
-            throw err;
-        }
-    }
-    fs.writeFile(TOKEN_DIR + "/access_token.json", JSON.stringify(token));
-    console.log('Token stored to ' + TOKEN_DIR);
+	try {
+		fs.mkdirSync(TOKEN_DIR);
+	} catch (err) {
+		if (err.code != 'EEXIST') {
+			throw err;
+		}
+	}
+	fs.writeFile(TOKEN_DIR + '/access_token.json', JSON.stringify(token));
+	console.log('Token stored to ' + TOKEN_DIR);
 }
