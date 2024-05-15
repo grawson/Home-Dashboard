491c4d3a7f87067449af3cbf891527f951c41357
Yoni Rawson <yrawson96@gmail.com>
2024-05-16 00:21:17 +0300

    allow for multiple minchas

--- _script/shabbatCard.js
+++ _script/shabbatCard.js
@@ -1,81 +1,102 @@
-const Mustache = require("mustache");
+const Mustache = require('mustache');
 require('datejs');
-const Cheerio = require('cheerio')
+const Cheerio = require('cheerio');
 const Axios = require('axios');
-const { getNextDayOfWeek, getFormattedDateForYINR } = require("./tools/date");
-const REFRESH_RATE = 60000;  // seconds
+const { getNextDayOfWeek, getFormattedDateForYINR } = require('./tools/date');
+const REFRESH_RATE = 60000; // seconds
 
 const TEMPLATE = './_view/shabbatCard.mustache';
 
 const scrapeData = async callback => {
-  var parshaData = await $.get('https://www.hebcal.com/shabbat/?cfg=json&zip=10804&m=0')
+	var parshaData = await $.get(
+		'https://www.hebcal.com/shabbat/?cfg=json&zip=10804&m=0'
+	);
 
-  const parsha = parshaData.items.find(e => e.category == 'parashat').title
+	const parsha = parshaData.items.find(e => e.category == 'parashat').title;
 
-  const holidays = [];
-  const fridayDate  = getNextDayOfWeek(new Date(), 5);
-  const fridayDateString = fridayDate && getFormattedDateForYINR(fridayDate);
-  const saturdayDate  = getNextDayOfWeek(new Date(), 6);
-  const saturdayDateString = saturdayDate && getFormattedDateForYINR(saturdayDate);
+	const holidays = [];
+	const fridayDate = getNextDayOfWeek(new Date(), 5);
+	const fridayDateString = fridayDate && getFormattedDateForYINR(fridayDate);
+	const saturdayDate = getNextDayOfWeek(new Date(), 6);
+	const saturdayDateString =
+		saturdayDate && getFormattedDateForYINR(saturdayDate);
 
-  var webpage = await Axios.get(`https://www.yinr.org/calendar?advanced=Y&calendar=&date_start=specific+date&date_start_x=0&date_start_date=${fridayDateString}&has_second_date=Y&date_end=specific+date&date_end_x=0&date_end_date=${saturdayDateString}&view=day&day_view_horizontal=N`);
+	const url = `https://www.yinr.org/calendar?advanced=Y&calendar=&date_start=specific+date&date_start_x=0&date_start_date=${fridayDateString}&has_second_date=Y&date_end=specific+date&date_end_x=0&date_end_date=${saturdayDateString}&view=day&day_view_horizontal=N`;
 
-  const html = Cheerio.load(webpage.data);
+	var webpage = await Axios.get(url);
 
-  const htmlText = html.text();
+	const html = Cheerio.load(webpage.data);
 
-  // candle lighting
-  const candleLightingMatch = htmlText.match(/\b(\d{1,2}:\d{2}(?:am|pm))\s+Candle Lighting\b/i);
-  
-  const candleLighting = candleLightingMatch && candleLightingMatch[1];
+	const htmlText = html.text();
 
-  holidays.push({
-    title: 'Candle Lighting',
-    time: candleLighting
-  });
+	// candle lighting
+	const candleLightingMatch = htmlText.match(
+		/\b(\d{1,2}:\d{2}(?:am|pm))\s+Candle Lighting\b/i
+	);
 
-  // mincha times
-  const minchaMatches = htmlText.match(/\b\d{1,2}:\d{2}(?:am|pm)\s+Mincha\b/ig)
-  
-  const friMinchaMatch = minchaMatches && minchaMatches[0].match(/\b\d{1,2}:\d{2}(?:am|pm)/);
-  const friMincha = friMinchaMatch && friMinchaMatch[0];
-  const shabMinchaMatch = minchaMatches && minchaMatches[1].match(/\b\d{1,2}:\d{2}(?:am|pm)/);
-  const shabMincha = shabMinchaMatch && shabMinchaMatch[0];
+	const candleLighting = candleLightingMatch && candleLightingMatch[1];
 
-  holidays.push({
-    title: 'Fri Mincha',
-    time: friMincha
-  });
+	holidays.push({
+		title: 'Candle Lighting',
+		time: candleLighting,
+	});
 
-  holidays.push({
-    title: 'Shab Mincha',
-    time: shabMincha
-  });
+	// mincha times
+	const days = html('.calendar_day_view');
 
-  // havdala
-  const havdalaMatch = htmlText.match(/\b(\d{1,2}:\d{2}(?:am|pm))\s+Shabbat Ends\b/i);
+	for (let i = 0; i < (days || []).length; i++) {
+		const dayContainer = days[i];
 
-  const havdala = havdalaMatch && havdalaMatch[1];
+		const dayText = html(dayContainer).text();
+		const minchaMatches = dayText.match(
+			/\b\d{1,2}:\d{2}(?:am|pm)\s+Mincha\b/gi
+		);
 
-  holidays.push({
-    title: 'Havdalah',
-    time: havdala
-  });
+		const minchaTimeRegex = /\b\d{1,2}:\d{2}(?:am|pm)/;
 
-  callback({ holidays, parsha });
-}
+		const times = [];
+		for (const match of minchaMatches || []) {
+			const timeMatch = match && match.match(minchaTimeRegex);
+
+			if (timeMatch) {
+				times.push(timeMatch);
+			}
+		}
+
+		const title = i === 0 ? 'Fri Mincha' : 'Shab Mincha';
+
+		holidays.push({
+			title,
+			time: times.join('/'),
+		});
+	}
+
+	// havdala
+	const havdalaMatch = htmlText.match(
+		/\b(\d{1,2}:\d{2}(?:am|pm))\s+Shabbat Ends\b/i
+	);
+
+	const havdala = havdalaMatch && havdalaMatch[1];
+
+	holidays.push({
+		title: 'Havdalah',
+		time: havdala,
+	});
+
+	callback({ holidays, parsha });
+};
 
 function render(data) {
-  $.get(TEMPLATE, template => {
-    var rendered = Mustache.render(template, data);
-    $('#shabbat-card').html(rendered);
-  });
+	$.get(TEMPLATE, template => {
+		var rendered = Mustache.render(template, data);
+		$('#shabbat-card').html(rendered);
+	});
 }
 
 $(function () {
-  scrapeData(render);
-  window.setInterval(function () {
-    console.log("Reloading calendar data...");
-    scrapeData(render);
-  }, REFRESH_RATE * 1000);
+	scrapeData(render);
+	window.setInterval(function () {
+		console.log('Reloading calendar data...');
+		scrapeData(render);
+	}, REFRESH_RATE * 1000);
 });
