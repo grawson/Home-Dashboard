3e54fd9e73cc0042baca78bfe8f1a4610247c3a2
Yoni Rawson <yrawson96@gmail.com>
2026-05-04 18:47:03 +0300

    reeenable all cards

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,7 @@
+import CalendarCard from '#/components/CalendarCard'
 import HolidaysCard from '#/components/HolidaysCard'
+import ShabbatTimesCard from '#/components/ShabbatTimesCard'
+import WeatherCard from '#/components/WeatherCard'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: App })
@@ -7,12 +10,12 @@ function App() {
   return (
     <main>
       <div className="bg-background flex h-screen w-screen gap-5 p-5">
-        {/* <CalendarCard /> */}
+        <CalendarCard />
 
         <div className="flex shrink-0 grow-0 basis-[270px] flex-col gap-5">
-          {/* <WeatherCard /> */}
+          <WeatherCard />
           <HolidaysCard />
-          {/* <ShabbatTimesCard /> */}
+          <ShabbatTimesCard />
         </div>
       </div>
     </main>
